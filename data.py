db50431256cb95fac567bbf2041fc9b8f125cecc
ifnuu01 <ifnuu01@gmail.com>
2023-10-29 08:27:14 +0800

    Merubah nama variable

--- data.py
+++ data.py
@@ -3,9 +3,9 @@ Login = {
 }
 
 
-Database = {"Samasung"    : [["Samsung J2 Prime",2,16,"Snapdragon 888",200000],
+Database = {"Samsung"    : [["Samsung J2 Prime",2,16,"Snapdragon 888",200000],
                              ["Samsung Galaxy note",8,256,"Snapdragon 729",4500000]],
-            "Infinix"     : [["Infinix Hot 11",6,128,"MediaTek Helio G88",24000000]]
+            "Infinix"     : [["Infinix Hot 11",6,128,"MediaTek Helio G88",24000000],]
 }
 
 header = ["ID","Type HandPhone","Ram (Gb)","Storage (Gb)","Processor","Harga (Rp)"]
