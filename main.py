f96bdbd88670346c4fc06eee6a67be8c2f6ad9b3
ifnuu01 <ifnuu01@gmail.com>
2023-10-29 17:46:14 +0800

    Menambahkan fitur edit

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from data import Login,Database,header
 from tabulate import tabulate
 
 #Fungsi untuk membersihkan terminal
-def Clear_screen():
+def clear_screen():
     #Untuk os windows cls , dan linux clear
     os.system('cls' if os.name == 'nt' else 'clear')
 
 #Fungsi login
-def Auth_role():
+def auth_role():
     gagal = 0
     #Jika gagal login 3x maka while akan berhenti
     while gagal < 3:
@@ -16,12 +16,12 @@ def Auth_role():
         #variable username dan password untuk menampung inputan
         username = input("Username : ")
         password = input("password : ")
-        Clear_screen()  
+        clear_screen()  
         #Jika username dan password terdetect sebagai admin maka akan masuk ke menu admin
         if username == "admin" and password == "admin":
-            Show_menu_admin()
+            show_menu_admin()
         #Jika terdetect sebagai user maka masuk ke menu user
-        elif username in Login and password == Login[username]:
+        elif username in Login.keys() and password == Login[username][0]:
             print("yes  user")
         else:
             #Jika username dan password tidak ada di dalam role user / admin maka variable gagal akan di tambah 1 sebagai pembatas perulangan
@@ -29,16 +29,23 @@ def Auth_role():
             print(f"noted :  Login gagal ke {gagal} , jika gagal login 3x maka akan di keluarkan ke menu awal\n")
 
 #Fungsi registrasi user
-def Registrasi_user():
-    print("Silahkan buat akun baru anda \n")
-    #variable username dan password untuk menampung inputan
-    username = input("Username : ")
-    password = input("password : ")
-    #Menambahkan username dan password ke dalam variable login atau bisa di sebut sebagai role user baru
-    Login.update({username : password})
+def registrasi_user():
+    while True:
+        print("Silahkan buat akun baru anda \n")
+        #variable username dan password untuk menampung inputan
+        username = input("Username baru : ")
+        if username in Login.keys():
+            clear_screen()
+            print("Noted : Username telah di pakai , coba yang lain\n")
+        else:
+            password = input("password baru : ")
+            clear_screen()
+            #Menambahkan username dan password ke dalam variable login atau bisa di sebut sebagai role user baru
+            Login.update({username : [password,0,[]]})
+            break
 
 #Fungsi Show menu awal yaitu fungsi yang pertama di lihat oleh pengguna
-def Show_menu_awal():
+def show_menu_awal():
     #Perulangan yang di mana menu akan terus di ulang jika tidak sesuai dengan inputan
     while True:
         #Menampilkan menu-menu yang terdapat pada menu awal
@@ -55,13 +62,13 @@ Exit        >> 3
 """)
         #Varible pilih sebagai penampung inputan
         pilih = input("Pilih Menu : ")
-        Clear_screen()
-        #Percabangan jika inputan pilih bernilai str 1 maka akan ke menu Auth_role
+        clear_screen()
+        #Percabangan jika inputan pilih bernilai str 1 maka akan ke menu auth_role
         if  pilih == "1":
-            Auth_role()
+            auth_role()
         #Jika inputan pilih bernilai str 2 maka akan ke menu Registrasi_useer
         elif pilih == "2":
-            Registrasi_user()
+            registrasi_user()
         #Jika inputan pilih bernilai str 3 maka memberhentikan program
         elif pilih == "3":
             print("Terima kasih telah berkunjung")
@@ -71,7 +78,7 @@ Exit        >> 3
             print("Noted : Pilih angka yang terdapat pada menu !")
 
 #Fungsi show menu admin
-def Show_menu_admin():
+def show_menu_admin():
     #Perulangan yang di mana menu akan terus di ulang jika tidak sesuai dengan inputan
     while True:
         #Menampilkan menu-menu yang terdapat pada menu admin
@@ -93,45 +100,48 @@ Logout           >> 5
 """)
         #Varible pilih sebagai penampung inputan
         pilih = input("Pilih menu : ")
-        Clear_screen()
-        #Percabangan yang dimana ketika inputan bernilai str 1 maka akan masuk ke Fungsi Read_handphone
+        clear_screen()
+        #Percabangan yang dimana ketika inputan bernilai str 1 maka akan masuk ke Fungsi read_handphone
         if  pilih == "1":
-            Read_handphone()
-        #Jika inputan pilih bernilai str 2 maka akan masuk ke Fungsi Update_handphone
+            read_handphone()
+        #Jika inputan pilih bernilai str 2 maka akan masuk ke Fungsi update_handphone
         elif pilih == "2":
-            Update_handphone()
-        #Jika inputan pilih bernilai str 3 maka akan masuk ke Fungsi Edit_handphone
+            update_handphone()
+        #Jika inputan pilih bernilai str 3 maka akan masuk ke Fungsi edit_handphone
         elif pilih == "3":
-            print("Terima kasih telah berkunjung")
-            exit()
+            edit_handphone()
         #Jika inputan pilih bernilai str 4 maka akan masuk ke Fungsi Delete_handphone
         elif pilih == "4":
             print("d")
-        #Jika inputan pilih bernilai str 5 maka akan masuk ke Fungsi Show_menu_awal
+        #Jika inputan pilih bernilai str 5 maka akan masuk ke Fungsi show_menu_awal
         elif pilih == "5":
-            Show_menu_awal()
+            show_menu_awal()
         #Jika akan inputan pilih tidak terdapat pada menu maka akan di berikan peringatan seperti di bawah
         else:
             print("Noted : Pilih angka yang terdapat pada menu !")
 
-def Back_to_menu():
+def back_to_menu():
     input("\nTekan enter untuk kembali...")
 
-def Read_handphone():
+def read_handphone():
     while True:
-        Menu_read()
+        menu_read()
         pilih = input("Pilih no brand : ")
-        Clear_screen()
+        clear_screen()
         if pilih == "1":
-            Show_type_samsung()
+            show_type_samsung()
+            back_to_menu()
+            clear_screen()
         elif pilih == "2":
-            Show_type_infinix()
+            show_type_infinix()
+            back_to_menu()
+            clear_screen()
         elif pilih == "3":
-            Show_menu_admin()
+            show_menu_admin()
         else:
             print("Noted : Pilih angka yang terdapat pada menu !")
 
-def Menu_read():
+def menu_read():
     print("""
 ==============
  Daftar Brand 
@@ -143,40 +153,36 @@ ______________
 Back    >> 3
 """)
 
-def Show_type_samsung():
+def show_type_samsung():
     Samsung = Database["Samsung"]
     print("Brand Samsung")
     for i in range(len(Samsung)):Samsung[i].insert(0,i+1)
     print(tabulate(Samsung,headers=header,tablefmt="simple_grid"))
     for i in range(len(Samsung)):del Samsung[i][0]
-    Back_to_menu()
-    Clear_screen()
 
-def Show_type_infinix():
+def show_type_infinix():
     Infinix = Database["Infinix"]
     print("Brand Infinix")
     for i in range(len(Infinix)):Infinix[i].insert(0,i+1)
     print(tabulate(Infinix,headers=header,tablefmt="simple_grid"))
     for i in range(len(Infinix)):del Infinix[i][0]
-    Back_to_menu()
-    Clear_screen()
 
 
-def Update_handphone():
+def update_handphone():
     while True:
-        Menu_read()
+        menu_read()
         Brand = input("Pilih brand yang mau di tambah : ")
-        Clear_screen()
+        clear_screen()
         if Brand == "1":
-            Update_samsung()
+            update_samsung()
         elif Brand =="2":
-            Update_infinix()
+            update_infinix()
         elif Brand == "3":
-            Show_menu_admin()
+            show_menu_admin()
         else:
             print("Noted : Pilih angka yang terdapat pada menu !")
 
-def Input_update():
+def input_update():
     while True:
         try:
             Type_handphone = input("Masukan type handphone : ")
@@ -184,28 +190,89 @@ def Input_update():
             Storage        = int(input("Storage / Penyimpanan (Gb): "))
             Processor      = input("Processor : ")
             Harga          = int(input("Harga : Rp"))
+            print("Noted : Data berhasil di tambahkan :) ")
             return [Type_handphone,Ram,Storage,Processor,Harga]
         except ValueError:
-            Clear_screen()
+            clear_screen()
             print("Noted : Inputan tidak valid silahkan mengulang \n")
 
 
-def Update_samsung():
+def update_samsung():
     Samsung        = Database["Samsung"]
-    Update         = Input_update()
+    Update         = input_update()
     Samsung.append(Update)
-    print("Noted : Data berhasil di tambahkan :) ")
-    Update_handphone()
+    update_handphone()
 
-def Update_infinix():
+def update_infinix():
     Infinix        = Database["Infinix"]
-    Update         = Input_update()
+    Update         = input_update()
     Infinix.append(Update)
-    print("Noted : Data berhasil di tambahkan :) ")
-    Update_handphone()
+    update_handphone()
     
+def edit_handphone():
+    while True:
+        menu_read()
+        Brand = input("Pilih brand yang mau diedit : ")
+        clear_screen()
+        if Brand == "1":
+            edit_samsung()
+        elif Brand =="2":
+            edit_infinix()
+        elif Brand == "3":
+            show_menu_admin()
+        else:
+            print("Noted : Pilih angka yang terdapat pada menu !")
+
+def edit_samsung():
+    while True:
+        try:
+            Samsung = Database["Samsung"]
+            show_type_samsung()
+            pilih = int(input("Pilih ID yang mau di edit : "))
+            clear_screen()
+            if pilih in [i+1 for i in range(len(Samsung))]:
+                clear_screen()
+                print("Masukan data baru \n ")
+                print("=====================")
+                Edit    = input_update()
+                clear_screen()
+                print("Noted : data berhasil di edit")
+                Samsung[pilih - 1] = Edit
+                return
+            else:
+                print("Noted : ID yang anda inputkan tidak ada\n")
+        except ValueError: 
+            clear_screen()
+            print("Noted : ID yang anda inputkan tidak ada\n")
+
+def edit_infinix():
+    while True:
+        try:
+            Infinix = Database["Infinix"]
+            show_type_infinix()
+            pilih = int(input("Pilih ID yang mau di edit : "))
+            clear_screen()
+            if pilih in [i+1 for i in range(len(Infinix))]:
+                clear_screen()
+                print("Masukan data baru \n ")
+                print("=====================")
+                Edit    = input_update()
+                clear_screen()
+                print("Noted : data berhasil di edit")
+                Infinix[pilih - 1] = Edit
+                return
+            else:
+                print("Noted : ID yang anda inputkan tidak ada\n")
+        except ValueError: 
+            clear_screen()
+            print("Noted : ID yang anda inputkan tidak ada\n")
+
+
+
+
+
 
 if __name__ == "__main__":
     os.system('pip install tabulate')
-    Clear_screen()
-    Show_menu_awal()
+    clear_screen()
+    show_menu_awal()
