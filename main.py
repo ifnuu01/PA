c85c026b0daea704280040408d3608a93043fc6d
ifnuu01 <ifnuu01@gmail.com>
2023-10-30 18:52:23 +0800

    Menambahkan role user

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from data import Login,Database,header,Samsung,Infinix
+from data import Login,header,Samsung,Infinix,header_keranjang
 from tabulate import tabulate
 
 #Fungsi untuk membersihkan terminal
@@ -22,7 +22,7 @@ def auth_role():
             show_menu_admin()
         #Jika terdetect sebagai user maka masuk ke menu user
         elif username in Login.keys() and password == Login[username][0]:
-            print("yes  user")
+            show_menu_user(username)
         else:
             #Jika username dan password tidak ada di dalam role user / admin maka variable gagal akan di tambah 1 sebagai pembatas perulangan
             gagal += 1
@@ -129,11 +129,11 @@ def read_handphone():
         pilih = input("Pilih no brand : ")
         clear_screen()
         if pilih == "1":
-            print(show_type(Samsung,"Samsung"))
+            print(show_type(Samsung,"Samsung",header))
             back_to_menu()
             clear_screen()
         elif pilih == "2":
-            print(show_type(Infinix,"Infinix"))
+            print(show_type(Infinix,"Infinix",header))
             back_to_menu()
             clear_screen()
         elif pilih == "3":
@@ -153,8 +153,8 @@ ______________
 Back    >> 3
 """)
 
-def show_type(brand,name_brand):
-    print(f"Brand {name_brand}")
+def show_type(brand,name_brand,header):
+    print(f"Daftar {name_brand}")
     for i in range(len(brand)):brand[i].insert(0,i+1)
     table = tabulate(brand,headers=header,tablefmt="simple_grid")
     for i in range(len(brand)):del brand[i][0]
@@ -215,10 +215,10 @@ def edit_handphone():
 def edit_type(brand,name_brand):
     while True:
         try:
-            print(show_type(brand,name_brand))
+            print(show_type(brand,name_brand,header))
             pilih = int(input("Pilih ID yang mau di edit : "))
             clear_screen()
-            if pilih in [i+1 for i in range(len(brand))]:
+            if pilih-1 < len(brand):
                 print("Masukan data baru \n ")
                 print("=====================")
                 edit = input_update()
@@ -249,10 +249,10 @@ def delete_handphone():
 def delete_type(brand,name_brand):
     while True:
         try:
-            print(show_type(brand,name_brand))
+            print(show_type(brand,name_brand,header))
             pilih = int(input("Pilih ID yang mau di hapus : "))
             clear_screen()
-            if pilih in [i+1 for i in range(len(brand))]:
+            if pilih-1 < len(brand):
                 del brand[pilih-1]
                 print("Noted : data berhasil di hapus")
                 return
@@ -261,6 +261,154 @@ def delete_type(brand,name_brand):
         except ValueError:
             clear_screen()
             print("Noted : ID yang anda inputkan tidak ada\n")
+
+def show_menu_user(nama_akun):
+    while True:
+        print(f"""
+=====================
+              
+    Dashboard User
+      
+=====================
+Isi saldo        >> 1
+_____________________
+list Produk      >> 2
+_____________________
+Keranjang        >> 3
+_____________________
+Log Out          >> 4
+_____________________
+Exit             >> 5
+_____________________
+Saldo anda saat ini >> Rp.{Login[nama_akun][1]}
+""")
+        pilihan_user = input("Pilih menu: ")
+        clear_screen()
+        if pilihan_user == "1":
+            top_up(nama_akun)
+        elif pilihan_user == "2":
+            read_handphone_user(nama_akun)
+        elif pilihan_user == "3":
+            keranjang(nama_akun)
+        elif pilihan_user == "4":
+            show_menu_awal()
+        elif pilihan_user == "5":
+            print("Terima kasih telah berkunjung")
+            exit()
+        else:
+            print("Noted : Pilih angka yang terdapat pada menu !")
+
+def top_up(nama_akun):
+    while True:
+        try:
+            print(f"Saldo anda saat ini berjumlah {Login[nama_akun][1]}\n")
+            top_up = int(input("Masukan nominal top up : Rp")) 
+            clear_screen()
+            Login[nama_akun][1] += top_up
+            print("Noted : Anda berhasil top up")
+            back_to_menu()
+            clear_screen()
+            show_menu_user(nama_akun)
+        except ValueError:clear_screen();print("Inputan anda tikak valid")
+
+def read_handphone_user(nama_akun):
+    while True:
+        menu_read()
+        pilih = input("Pilih no brand : ")
+        clear_screen()
+        if pilih == "1":
+            show_type_user(Samsung,"Samsung",nama_akun)
+        elif pilih == "2":
+            show_type_user(Infinix,"Infinix",nama_akun)
+        elif pilih == "3":
+            show_menu_user(nama_akun)
+        else:
+            print("Noted : Pilih angka yang terdapat pada menu !")
+
+def show_type_user(brand,name_brand,nama_akun):
+    while True:
+        print(show_type(brand,name_brand,header))
+        pilihan = input("ingin memasukan produk ke keranjang [y/n] :")
+        clear_screen()
+        if pilihan == "y":
+            masuk_keranjang(brand,name_brand,nama_akun)
+             #menambahkan ke keranjang
+        elif pilihan =="n":
+            read_handphone_user(nama_akun)
+        else:
+            print("Noted : Pilih y / n !! , Silahkan ulangi\n")
+
+def masuk_keranjang(brand,name_brand,nama_akun):
+    while True:
+        try:
+            print(show_type(brand,name_brand,header))
+            pilih = int(input("Masukan ID handphone yang mau di masukan ke keranjang : "))
+            clear_screen()
+            if pilih -1 < len(brand):
+                produk = brand[pilih - 1][0]
+                harga = brand[pilih -1 ][4]
+                keranjang = [produk,harga]
+                Login[nama_akun][2].append(keranjang)
+                print("Data berhasil di masukan ke keranjang\n")
+                back_to_menu()
+                show_menu_user(nama_akun)
+            else:
+                print("Noted : ID yang anda inputkan tidak ada\n")
+        except ValueError:clear_screen();print("Noted : ID yang anda inputkan tidak ada\n")
+
+def keranjang(nama_akun):
+    while True:
+        keranjang_akun = Login[nama_akun][2]
+        print(show_type(keranjang_akun,"Keranjang",header_keranjang))
+        Pilihan_checkout= input("Apakah ada barang yang ingin di check out[y/n]: ")
+        clear_screen()
+        if  Pilihan_checkout == "y":
+            Check_out(nama_akun, keranjang_akun)
+        elif Pilihan_checkout == "n":
+            show_menu_user(nama_akun)
+        else:
+            print("Noted : Pilih y / n !!, Silahkan ulangi lagi\n")
+
+#Fungsi Checkout
+def Check_out(nama_akun, keranjang_akun):
+    while True: 
+        print(show_type(keranjang_akun,"Keranjang",header_keranjang))
+        ID_checkout = int(input("Masukkan ID hanphone yang mau di check out: "))
+        clear_screen()
+        if ID_checkout-1 < len(Login[nama_akun][2]):        
+            barang_checkout= Login[nama_akun][2][ID_checkout-1]
+            print(f"Saldo anda saat ini adalah Rp. {Login[nama_akun][1]}\n")
+            Transaksi = input("Apakah anda ingin melanjutkan transaksi(y/n): ")
+            clear_screen()
+            if Transaksi == "y":
+                if Login[nama_akun][1] < barang_checkout[1]:
+                    print("Noted : Saldo Tidak Cukup\n")
+                    back_to_menu()
+                    show_menu_user(nama_akun)
+                else:
+                    sisa_saldo = Login[nama_akun][1]-barang_checkout[1]
+                    Login[nama_akun][1] = sisa_saldo
+                    print (f"""
+Selamat Pembayaran Anda Berhasil
+================================
+Nama Produk   : 
+{Login[nama_akun][2][ID_checkout-1][0]}
+________________________________
+Total Harga   :
+Rp {Login[nama_akun][2][ID_checkout-1][1]}
+________________________________
+""")
+                    del Login[nama_akun][2][ID_checkout-1]
+                    back_to_menu()
+                    show_menu_user(nama_akun)
+            elif Transaksi =="n":
+                print("Transaksi gagal")
+                back_to_menu()
+                show_menu_user(nama_akun)
+            else:
+                print("Noted : Pilih y / n !! Silahkan ulangi \n")
+        else:
+            print("Noted : Masukan ID yang tertera pada keranjang , Silahkan ulangi dari awal\n")
 
 
 if __name__ == "__main__":
