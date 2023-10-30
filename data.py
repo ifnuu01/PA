b2426a91dad63d354c8817eb88f4a51e5c318d20
ifnuu01 <ifnuu01@gmail.com>
2023-10-30 18:52:49 +0800

    Penambahan variable dan alias

--- data.py
+++ data.py
@@ -3,13 +3,14 @@ Login = {
     "user"  : ["user",0,[]] #Index 0 sebagai pass , index 1 sebagai saldo , dan 2 sebagai keranjang
 }
 
-
 Database = {"Samsung"    : [["Samsung J2 Prime",2,16,"Snapdragon 888",200000],
                              ["Samsung Galaxy note",8,256,"Snapdragon 729",4500000]],
             "Infinix"     : [["Infinix Hot 11",6,128,"MediaTek Helio G88",24000000],]
 }
 
 header = ["ID","Type HandPhone","Ram (Gb)","Storage (Gb)","Processor","Harga (Rp)"]
+
+header_keranjang = ["ID", "Tipe HP", "Harga"]
 
 Samsung = Database["Samsung"]
 Infinix = Database["Infinix"]
